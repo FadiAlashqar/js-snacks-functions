7ac925f8a68f515099050e41c0d97a3e0828280b
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-25 00:26:27 +0100

    [UPD] ex 2 done

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,24 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+let userInitials = []
 
 // Dichiara la funzione qui.
+function capitalizeFisrsLetter() {
+    for (let i = 0; i < names.length; i++) {
+        let singleNames = names[i].split()
+        for (let j = 0; j < singleNames.length; j++) {
+            let initials = singleNames[j].charAt(0)
+            userInitials.push(initials)
+            
+        }
+    }
+    return userInitials
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
-
+ const result = capitalizeFisrsLetter(names, userInitials);
+ console.log(result)
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
