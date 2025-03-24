dfbcccb7dd0d7b567dd1f39824bd99667a8ceb0f
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-24 23:34:30 +0100

    [UPD] ex 1.2 done

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,16 +1,18 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const userName = 'Mario';
+console.log(userName)
 
 
 // Dichiara la funzione qui.
-function name(personName) {
-    return `ciao ${personName}`
+function greetUser(personName) {
+    return `ciao ${personName}!`
 
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+greetUser(userName);
+console.log(greetUser(userName));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
