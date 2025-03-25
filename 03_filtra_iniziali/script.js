7ac2b4b4095853eb5ea5db894f93808349f374c6
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-25 01:43:43 +0100

    [UPD] ex 3 done

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,23 +1,23 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-const firstLetter = []
+const firstLetter = [];
 
 // Dichiara la funzione qui.
 function capitalLetters(names, firstLetter) {
     for (let i = 0; i < names.length; i++) {
         if (names[i].charAt(0) === 'A') {
-            firstLetter.push(names[i])
+        firstLetter.push(names[i]);
         }
         
     }
-    return firstLetter
+    return firstLetter;
 }
 
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
-capitalLetters(names, firstLetter)
+const result = capitalLetters(names, firstLetter);
+console.log(result);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
