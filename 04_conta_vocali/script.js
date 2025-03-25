647679c152d14ce4ec5a5993c58484e410dd2c5e
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-25 02:38:34 +0100

    [UPD] ex 4 done

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,30 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
-
+let vowels = ["a", "e", "i", "o", "u"];
+let result = '';
+let singleLetters = word.split('');
 // Dichiara la funzione qui.
+
+function findVowels(singleLetters, vowels) {
+    for (let i = 0; i < singleLetters.length; i++) {
+        
+        for (let j = 0; j < vowels.length; j++) {
+            if (singleLetters[i] === vowels[j]) {
+                result+= vowels[j]
+            }
+            
+        }
+        
+    }
+    let vowelsNum = result.length;
+    return {result, vowelsNum}
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const solution = findVowels(singleLetters, vowels);
+console.log(solution)
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
